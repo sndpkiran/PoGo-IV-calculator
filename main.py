318545aa92facda087f9f0c6df2b764d629a5d46
sndpkiran <sndpkiran@gmail.com>
2017-02-04 19:33:18 +0530

    crops values from the image

---
+++ main.py
@@ -0,0 +1,17 @@
+import pytesseract
+from PIL import Image
+import os
+
+img = Image.open("rainer.jpeg")
+w, h = img.size
+img.crop((50, 50, 700, 150)).save("cp.jpg")
+img.crop((200, 600, 500, 650)).save("hp.jpg")
+img.crop((300, 850, 450, 950)).save("sd.jpg")
+
+print(pytesseract.image_to_string(Image.open("hp.jpg")))
+print(pytesseract.image_to_string(Image.open("cp.jpg")))
+print(pytesseract.image_to_string(Image.open("sd.jpg")))
+
+os.remove("cp.jpg")
+os.remove("hp.jpg")
+os.remove("sd.jpg")
